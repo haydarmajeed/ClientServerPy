6f9869ce17158d6803bb8eb8663f58160d84116e
haydarmajeed <36525709+haydarmajeed@users.noreply.github.com>
2018-03-19 10:31:14 -0700

    Create udpserver.py

---
+++ udpserver.py
@@ -0,0 +1,29 @@
+#Server:
+
+import socket
+
+HOST = ''
+PORT = 4040
+
+def create_listen_socket(host, port):
+    """ Setup the sockets our server will receive connection requests on """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((host, port))
+    sock.listen(100)
+    return sock
+
+def recv_msg(sock):
+    """ Wait for data to arrive on the socket, then parse into messages using b'\0' as message delimiter """
+    data = bytearray()
+    msg = ''
+    # Repeatedly read 4096 bytes off the socket, storing the bytes
+    # in data until we see a delimiter
+    while not msg:
+        recvd = sock.recv(4096)
+        if not recvd:
+            # Socket has been closed prematurely
+            raise ConnectionError()
+        data = data + recvd
+        if b'\0' in recvd:
+            # we know from...
